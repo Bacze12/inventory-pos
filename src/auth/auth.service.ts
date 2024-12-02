a4e5d262e5cf350c1453f110e12ba8c2aadbfbd0
Bacze12 <brunocc.0205@gmail.com>
2024-12-02 20:55:47 -0300

    Explicitly exclude 'password' from user object in validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,11 +13,12 @@ export class AuthService {
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.prisma.user.findUnique({ where: { email } });
     if (user && (await bcrypt.compare(password, user.password))) {
-      const { ...result } = user; // Eliminamos 'password'
+      const { password: _password, ...result } = user; // Excluye 'password' explícitamente
       return result;
     }
     return null;
   }
+  
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
