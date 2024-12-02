05ecfab60c4613686e2e2cbfdd15b0887cb01e3b
Bacze12 <brunocc.0205@gmail.com>
2024-12-02 20:46:48 -0300

    Add PrismaClient and bcrypt for user seeding functionality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,8 @@
 // prisma/seed.ts
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+const prisma = new PrismaClient();
+
 async function main() {
     const adminUser = await prisma.user.upsert({
       where: { email: 'admin@example.com' },
