160e07a8233f8fa16d4d43f9231ee36b0f8bb2f6
Bacze12 <brunocc.0205@gmail.com>
2024-12-02 21:15:36 -0300

    Fix import path for CreateProductDto in products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { CreateProductDto } from '../products/dto/create-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
