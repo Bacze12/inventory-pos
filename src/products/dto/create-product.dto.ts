1b413a60e652507da0547649118756a269d9b1bd
Bacze12 <brunocc.0205@gmail.com>
2024-12-04 22:37:01 -0300

    Add `id` and `createdAt` properties to `CreateProductDto` class
    * Add `id` property with `ApiProperty` decorator
    * Add `createdAt` property with `ApiProperty` decorator

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty({ description: 'ID del producto', example: 1 })
+  public id: number;
+
   @ApiProperty({ description: 'Nombre del producto', example: 'Laptop' })
   public name: string;
 
   @ApiProperty({ description: 'Precio del producto', example: 1000.5 })
   public price: number;
+
+  @ApiProperty({ description: 'Fecha de creación del producto', example: new Date() })
+  public createdAt: Date;
 }
