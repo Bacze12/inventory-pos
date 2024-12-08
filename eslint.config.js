b07ff5f2981c473d55143b43513ff58b5c947e98
Bacze12 <brunocc.0205@gmail.com>
2024-12-07 22:39:59 -0300

    Add ESLint configuration and update dependencies for improved code quality

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettier,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
